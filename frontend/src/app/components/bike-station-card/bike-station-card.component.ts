135df72b4bb79cc968a89a746c61c5e01de1b515
jkalata <kalkuba95@gmail.com>
2021-09-04 14:08:07 +0200

    route changing performance fix

--- frontend/src/app/components/bike-station-card/bike-station-card.component.ts
+++ frontend/src/app/components/bike-station-card/bike-station-card.component.ts
@@ -1,18 +1,22 @@
 import { MapHeightService } from './../../services/map-height.service';
 import { IBikeStation } from '../../interfaces/bikes.interfaces';
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-bike-station-card',
   templateUrl: './bike-station-card.component.html',
   styleUrls: ['./bike-station-card.component.scss'],
 })
-export class BikeStationCardComponent {
+export class BikeStationCardComponent implements AfterViewInit {
   @Input() bikeStation: IBikeStation;
   usersLocation;
   constructor(private mapHeightService: MapHeightService) {}
 
-  changeHeight() {
+  ngAfterViewInit() {
+    this.changeHeight();
+  }
+
+  private changeHeight(): void {
     this.mapHeightService.setBikeStationCardHeight(
       document.getElementById('bike-container').offsetHeight
     );
